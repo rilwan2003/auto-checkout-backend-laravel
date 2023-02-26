34ba952eb8b91309a98ccdfb07cac9b5e06817bd
rilwan2003 <rilwan2003@gmail.com>
2023-02-26 15:50:44 +0100

    modified:   auto-checkout-backend-laravel/auto-checkout-laravel/database/migrations/2023_02_25_203036_create_checkouts_table.php

--- auto-checkout-backend-laravel/auto-checkout-laravel/database/migrations/2023_02_25_203036_create_checkouts_table.php
+++ auto-checkout-backend-laravel/auto-checkout-laravel/database/migrations/2023_02_25_203036_create_checkouts_table.php
@@ -1,4 +1,4 @@
-<?php
+// database/migrations/create_checkouts_table.php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -6,24 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCheckoutsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('checkouts', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('product_id');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('checkouts');
